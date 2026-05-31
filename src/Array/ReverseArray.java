ddca7883f44c68fbb0fbd8180e6ba0279f8162b4
prajjawalssipmt <tanmayprajjawalgupta@gamil.com>
2026-05-31 23:58:51 +0530

    add ReverseArray.java

---
+++ src/Array/ReverseArray.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class ReverseArray {
+    static void main() {
+        int[] arr=new int[]{1,2,3,4,5,6,7};
+
+        for (int i = 0; i < arr.length/2; i++) {
+            int temp = arr[i];
+            arr[i]=arr[arr.length-1-i];
+            arr[arr.length-1-i]=temp;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
