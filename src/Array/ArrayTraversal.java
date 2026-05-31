bf897265bc7592213fa9de264b4a42e4e5c91575
prajjawalssipmt <tanmayprajjawalgupta@gamil.com>
2026-05-31 23:12:46 +0530

    create new folder array add test,traversal file

---
+++ src/Array/ArrayTraversal.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class ArrayTraversal {
+    static void main() {
+        int[] arr = {11, 24, 38, 42, 59};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
