186664a859b81cfa9efcf56e0676e75d20a5b3a1
prajjawalssipmt <tanmayprajjawalgupta@gamil.com>
2026-06-01 22:23:57 +0530

    add findgcd and findlcm

--- src/InterviewQue/FindGCD.java
+++ src/InterviewQue/FindGCD.java
@@ -1,4 +1,23 @@
 package InterviewQue;
 
+import java.util.Scanner;
+
 public class FindGCD {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a num: ");
+        int a=sc.nextInt();
+        System.out.print("Enter b num: ");
+        int b=sc.nextInt();
+
+        int tempVal= Math.min(a, b);
+        while(tempVal>0){
+            if (a%tempVal==0 && b%tempVal==0){
+                System.out.println("GCD : "+ tempVal);
+                break;
+            }
+            tempVal--;
+        }
+
+    }
 }
--- src/InterviewQue/FindLCM.java
+++ src/InterviewQue/FindLCM.java
@@ -1,4 +1,23 @@
 package InterviewQue;
 
+import java.util.Scanner;
+
 public class FindLCM {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a num: ");
+        int a=sc.nextInt();
+        System.out.print("Enter b num: ");
+        int b=sc.nextInt();
+
+
+        int lcm=Math.min(a,b);
+        while (true){
+            if (lcm%a==0 && lcm%b==0){
+                System.out.println(lcm);
+                break;
+            }
+            lcm++;
+        }
+    }
 }
