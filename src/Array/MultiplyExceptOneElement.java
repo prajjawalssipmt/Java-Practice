4920006decbf31d1b95a59c43b798cfeb17d292c
prajjawalssipmt <tanmayprajjawalgupta@gamil.com>
2026-06-22 19:30:12 +0530

    add ArrayList

--- src/Array/MultiplyExceptOneElement.java
+++ src/Array/MultiplyExceptOneElement.java
@@ -1,4 +1,19 @@
 package Array;
 
 public class MultiplyExceptOneElement {
+    static void main() {
+        int[] arr=new int[]{1,3,5,7,9};
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==3){
+                continue;
+            }else {
+                arr[i]=arr[i]*2;
+            }
+        }
+
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
 }
--- src/ArrayList/TestArrayList.java
+++ src/ArrayList/TestArrayList.java
@@ -1,4 +1,62 @@
 package ArrayList;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InaccessibleObjectException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class TestArrayList {
+    static void main() throws NoSuchFieldException, IllegalAccessException, InaccessibleObjectException {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i)+" ");
+        }
+
+        //add
+        list.add(1,5);
+        System.out.println("\n"+ list);
+
+        //remove
+        list.remove(2);
+        System.out.println(list);
+
+        //add first and last
+        list.addFirst(6);
+        list.addLast(7);
+        System.out.println(list);
+
+        //check empty
+        System.out.println(list.isEmpty());
+
+        //set at index or replace
+        list.set(0,0);
+        System.out.println(list);
+
+        //find size
+        System.out.println(list.size());
+
+        //check element contains
+        System.out.println(list.contains(3));
+
+
+        List<Integer> list2 = List.of(5,6,7,8,9);
+        list.addAll(list2);
+        System.out.println(list);
+
+
+        Collections.sort(list);
+        System.out.println(list);
+
+        int frequency = Collections.frequency(list, 5);
+        System.out.println(frequency);
+
+        Collections.reverse(list);
+        System.out.println(list);
+    }
 }
